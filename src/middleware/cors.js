e25085541d47fb3232c2cddfbd8013a67e56cd6b
daoik <dimeconomu@gmail.com>
2024-03-30 18:36:39 +0200

    frontend env variable

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,7 +1,7 @@
 import cors from "cors";
-
+const URL = process.env.NEXT_PUBLIC_NEXT_PUBLIC_FRONT_URL;
 export const corsMiddleware = cors({
-  origin: "http://localhost:5174", // Change '*' to the specific origin you want to allow
+  origin: { URL }, // Change '*' to the specific origin you want to allow
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Add any other methods you need
   allowedHeaders: ["Content-Type", "Authorization"], // Add any other headers you want to allow
 });
